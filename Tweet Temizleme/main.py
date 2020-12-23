383ea33653cf56ec7910aa665fe7c07ab718249b
emrebinnaz <emre.binnaz@ceng.deu.edu.tr>
2020-12-23 18:53:40 +0300

    Tweet Cekme İşlemi Bitti

---
+++ Tweet Temizleme/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+tweets = pd.read_csv('ContentOfTweets.csv', sep = ",")
+
+def convertDataTypes():
+
+    tweets['id'] = tweets['id'].astype('string')
+    tweets['tweet'] = tweets['tweet'].astype('string')
+    tweets['label'] = tweets['label'].astype('category')
+
+    return tweets
+
+convertDataTypes()
+print(tweets.info())
