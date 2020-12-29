aba351eec63b70a03a82279361770fca5096ade7
emrebinnaz <emre.binnaz@ceng.deu.edu.tr>
2020-12-29 21:58:05 +0300

    Tweet Analiz v2

--- TweetAnaliz/AnalysisOfTweets.py
+++ TweetAnaliz/AnalysisOfTweets.py
@@ -1,5 +1,6 @@
 import pandas as pd
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 # from TweetTemizleme.Stopwords import *
 # from TweetTemizleme.main import *
 
@@ -7,10 +8,37 @@ import pandas as pd
 # saveCsv(original_tweets,originalTweetsPath)
 
 originalTweetsPath = 'Files/ContentOfTweets.csv'
-original_tweets = pd.read_csv(originalTweetsPath, sep=",", skipinitialspace=True)  # data frame oldu
+wordFrequenciesPath = 'Files/WordFrequencies.csv'
+tfidfPath = 'Files/tfidf.csv'
+wordFrequenciesBiggerThanFivePath = 'Files/WordFrequenciesBiggerThanFive.csv'
+
+original_tweets = pd.read_csv(originalTweetsPath, sep=",", skipinitialspace=True)
 
 def createFrequencyTable():
 
     freq_tweets = original_tweets['clean_text'].apply(lambda x: pd.value_counts(x.split(" "))).sum(axis = 0).reset_index()
     freq_tweets.columns = ['words', 'frequencies']
-    freq_tweets.to_csv('deneme.csv', index=None)
+    freq_tweets.to_csv(wordFrequenciesPath, index=None)
+
+def filterFrequencies():
+
+    word_frequencies = pd.read_csv(wordFrequenciesPath, sep=",", skipinitialspace=True)
+    filtered_frequencies = word_frequencies[word_frequencies['frequencies'] >= 5]
+    filtered_frequencies.to_csv(wordFrequenciesBiggerThanFivePath, index=None)
+
+def createTFIDFMatrix():
+
+    cleanTexts = original_tweets['clean_text']
+    vectorizer = TfidfVectorizer(analyzer = 'word', lowercase = True, min_df=10)
+
+    trainVector = vectorizer.fit_transform(cleanTexts)
+    tfidf = pd.DataFrame(trainVector.toarray(), columns = vectorizer.get_feature_names())
+    tfidf['label'] = original_tweets['label']
+    tfidf.to_csv(tfidfPath, index=None)
+
+# filterFrequencies()
+# createTFIDFMatrix()
+
+
+tfidf = pd.read_csv(tfidfPath, sep=",", skipinitialspace=True)
+print(tfidf.info())
