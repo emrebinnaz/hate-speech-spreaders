36d6f1161aa383d0c18bc19c3e8b1cbc6ddc3cb6
Emre <emre.binnaz@ceng.deu.edu.tr>
2021-04-01 00:30:01 +0300

    Future prediction for tfidf

---
+++ FeatureExtraction/FuturePrediction.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import joblib
+from ModelTraining.ModelFunctions import loadModel
+
+
+def prediction(modelName, newTextList):
+
+    model = loadModel("../ModelTraining/Models/", modelName)
+
+    tfidfVector = joblib.load(open("../Files/TfidfVector.pkl", "rb"))
+    # tfidfVector.min_df = 0
+
+    wordList = tfidfVector.get_feature_names() # columns
+
+    newSample = tfidfVector.transform(newTextList) # Transform dictionary features into 2D feature matrix.
+
+    newSampleAsDataFrame = pd.DataFrame(data = newSample.toarray(),
+                                        columns = wordList)
+
+    prediction = model.predict(newSampleAsDataFrame)
+
+    print(prediction)
+
+
+prediction('MultinomialNaiveBayes', ["love","I hate you"])
